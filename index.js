2b1a6c66d5b31a2b7796cbc57bcdf3a745037d91
agustinyuse <agustinyuse@gmail.com>
2023-03-25 00:19:46 -0300

    sin socketio

--- index.js
+++ index.js
@@ -10,35 +10,8 @@ dotenv.config();
 
 const cors = require("cors");
 
-const io = require("socket.io")(http, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-
 app.use(express.json());
 app.use(cors());
-
-io.on("connection", function (socket) {
-  console.log("a user connected");
-  socket.on("chat message", async function (msg) {
-    const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: msg }],
-    });
-
-    const message = {
-      prompt: msg,
-      content: completion.data.choices[0].message.content,
-    };
-
-    io.emit("chat message", message);
-  });
-  socket.on("disconnect", function () {
-    console.log("user disconnected");
-  });
-});
 
 const configuration = new Configuration({
   apiKey: process.env.openAIApiKey,
@@ -52,7 +25,13 @@ app.post("/chat", async (req, res) => {
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: message }],
   });
-  res.json(completion.data.choices[0].message.content);
+
+  const chat = {
+    prompt: message,
+    content: completion.data.choices[0].message.content,
+  };
+
+  res.json(chat);
 });
 
 const PORT = process.env.PORT || 3001;
